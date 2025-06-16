22c4b0b3d7d2f783771932c2bd9506d0666fffbd
0xav10086 <2320334524@qq.com>
2025-06-17 01:51:40 +0800

    feat: add router and home view.

---
+++ netmusic/netmusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  netmusicApp.swift
+//  netmusic
+//
+//  Created by Bemly on 2025/6/16.
+//
+
+import SwiftUI
+
+@main
+struct netmusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ netmusicTests/netmusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  netmusicTests.swift
+//  netmusicTests
+//
+//  Created by Bemly on 2025/6/16.
+//
+
+import Testing
+@testable import netmusic
+
+struct netmusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
